95258bd1405c486ca428ca930f027101a664d7c7
satish707 <satish.gangwar707@gmail.com>
2021-01-30 23:03:30 +0530

    Add files via upload

--- src/routing/fizzBuzz.js
+++ src/routing/fizzBuzz.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const FizzBuzzController = require('../controller/fizzBuzzController')
+const validateParams = require('../validator')
 
+router.get('/:count', validateParams.validateParams([
+    {
+        param_key: 'count',
+        required: true,
+        type: 'number',
+    }]), FizzBuzzController.getFizzBuzz);
+
+router.all('/', (req, res) => {
+    res.status(403).send('Route not found.')
+})
+
+module.exports = router;
