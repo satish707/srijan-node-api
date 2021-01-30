fd15fdf4221b3d7f881d00221a1def3bb61043f8
satish707 <satish.gangwar707@gmail.com>
2021-01-30 23:02:21 +0530

    Add files via upload

--- src/controller/fizzBuzzController.js
+++ src/controller/fizzBuzzController.js
@@ -1,1 +1,9 @@
+const getFizzBuzz = require('../Utill');
+const FizzBuzzController = {
+    getFizzBuzz: function (req, res) {
+        const data = getFizzBuzz(req.params.count);
+        res.send(data).status(200);
+    }
+}
 
+module.exports = FizzBuzzController;
